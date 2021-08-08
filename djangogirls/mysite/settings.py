c6dae2a5ebc4aa163d45f9879ba2f962e38a7533
l1storez <alesrov@gmail.com>
2021-08-08 22:20:33 +0300

    Add a secret key to the settings module

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -19,6 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = (
+    'django-insecure-^l!5x4qt'
+    'j7(j@*)y#^z^h*=%+c47tv3(#i-q5)_ap=n=irqzcn'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
