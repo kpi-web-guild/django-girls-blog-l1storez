4e90872453a95e8dda804a4561364e32d3a2565c
l1storez <alesrov@gmail.com>
2021-08-23 23:09:52 +0300

    Create a blog post model

--- djangogirls/blog/models.py
+++ djangogirls/blog/models.py
@@ -1,1 +1,26 @@
 """Model for blog."""
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+
+class Post(models.Model):
+    """Post model for blog."""
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        """Publish the post."""
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        """Render a text (string) with a Post title."""
+        return str(self.title)
