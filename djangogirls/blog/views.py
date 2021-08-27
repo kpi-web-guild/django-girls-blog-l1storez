3477cdf730575e569356ba37bd7849daddac049e
l1storez <alesrov@gmail.com>
2021-08-28 00:16:54 +0300

    fix d-template

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -7,4 +7,4 @@ from .models import Post
 def post_list(request):
     """Render post list on the webpage."""
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
