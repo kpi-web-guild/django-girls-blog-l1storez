ffcfade4fa3a6622221deb9a53718f0c7461b0d7
l1storez <alesrov@gmail.com>
2021-08-27 23:36:12 +0300

    add dynamic data in template

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,7 +1,10 @@
 """Views for blog."""
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 def post_list(request):
     """Render post list on the webpage."""
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
