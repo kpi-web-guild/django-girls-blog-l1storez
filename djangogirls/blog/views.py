90f6d5bfa0a79ef5ef8105064d23caac45143a75
l1storez <alesrov@gmail.com>
2021-08-29 00:37:33 +0300

    Create post_new view

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -14,3 +15,9 @@ def post_detail(request, pk):
     """Render post detail on the webpage."""
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    """Render new post on the webpage."""
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
